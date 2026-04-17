26d762ba04dcecd640993c9280a9cb387ef4cc10
nabilbaroq <nabilmuhamad89@gmail.com>
2026-04-17 10:25:02 +0700

    feat: integrate json-server and implement product management features
    - Added json-server as a dependency for mock API.
    - Created API utility functions for fetching, adding, and deleting products and categories.
    - Updated Home component to fetch and display products and categories.
    - Implemented product deletion functionality in the Home component.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,42 @@
-import Image from "next/image";
+"use client"
+
+import { useEffect, useState } from "react";
+import { getProduct , addProduct, deleteProduct ,getCategory } from "@/lib/api";
 
 export default function Home() {
+  const [ products, setProducts] = useState([])
+  const [ categories, setCategories] = useState([])
+
+  useEffect(() => {
+    fetchProducts();
+    fetchCategories()
+  }, [])
+
+  const fetchProducts = async () => {
+    const data = await getProduct()
+    setProducts(data)
+  }
+
+  const fetchCategories = async () => {
+    const data = await getCategory()
+    setCategories(data)
+  }
+
+
+
+  const handleDelete = async (id: number) => {
+    await deleteProduct(id)
+    fetchProducts()
+  }
+
   return (
     <div>
-      tes
+      {products.map((product: any) => (
+        <div key={product.id}>
+          <p>{product.name}</p>
+          <p>stock: {product.stock}</p>
+        </div>
+      ))}
     </div>
   );
 }
---
+++ src/lib/api.ts
@@ -0,0 +1,27 @@
+const BASE_URL = "http://localhost:3001"
+
+export async function getProduct() {
+    const res = await fetch(`${BASE_URL}/products`)
+    return res.json();
+}
+
+export async function addProduct(data: object) {
+    const res = await fetch(`${BASE_URL}/product`, {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(data),
+    })
+    return res.json()
+}
+
+export async function deleteProduct(id:number) {
+    const res = await fetch(`${BASE_URL}/product/${id}`, {
+        method: "DELETE"
+    })
+    return res.ok
+}
+export async function getCategory() {
+    const res = await fetch(`${BASE_URL}/categories`)
+    return res.json()
+    
+}
